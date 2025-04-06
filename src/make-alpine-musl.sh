d9ceb15d5adb1b9cb9d2161777b67b1eb2deccf6
Sumedh Wale <sumwale@gmail.com>
2025-04-07 05:15:04 +0530

    pass PRODUCT_VERSION for the static-musl build

--- src/make-alpine-musl.sh
+++ src/make-alpine-musl.sh
@@ -7,6 +7,6 @@ apk add glib-static openssl-libs-static zlib-static util-linux-static
 apk cache purge
 
 cd /build
-make all-static STATIC_LIBS="-lpcre2-8 -lffi -lz -lintl -lmount -lblkid -leconf"
+make all-static PRODUCT_VERSION="$1" STATIC_LIBS="-lpcre2-8 -lffi -lz -lintl -lmount -lblkid -leconf"
 strip -g *-`uname -m`-static
 chown --reference unlock.c *-static
