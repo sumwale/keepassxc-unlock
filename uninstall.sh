dab726409f44c7269dc652e738057fab7767f76f
laur89 <laur89@users.noreply.github.com>
2025-12-03 18:58:16 +0900

    Update un/install.sh scripts (#7)
    * install.sh: introduce bashisms, quote variables
      - resolve number of shellcheck issues, mostly around variable quoting
      - replace sh syntax of [ -o -a = ] with that of bash: [[ || && == ]]
      - systemctl enable & start -> enable --now
      - as our logic resolves user ID, it's necessary for the installation
        to run as said user and elevate required commands via sudo/doas
        as is already the case
      - use find instead of shell globbing
      - add -C to install command
    
    * uninstall.sh
      - remove unused doc_files
      - replace systemctl -q list-units by systemctl show
        (from https://unix.stackexchange.com/a/751774/47501)

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -7,11 +7,20 @@ fg_orange='\033[33m'
 fg_cyan='\033[36m'
 fg_reset='\033[00m'
 
-sbin_files="keepassxc-unlock-setup keepassxc-login-monitor keepassxc-unlock keepassxc-unlock-all"
-old_sbin_files="pam-keepassxc-auth"
-old_package="pam-keepassxc"
-service_files="keepassxc-login-monitor.service keepassxc-unlock@.service"
-doc_files="README.md LICENSE"
+sbin_files="
+    keepassxc-unlock-setup
+    keepassxc-login-monitor
+    keepassxc-unlock
+    keepassxc-unlock-all
+"
+old_sbin_files="
+    pam-keepassxc-auth
+"
+old_package='pam-keepassxc'
+service_files="
+    keepassxc-login-monitor.service
+    keepassxc-unlock@.service
+"
 config_dir=/etc/keepassxc-unlock
 
 # ensure that system PATHs are always searched first
@@ -22,35 +31,35 @@ echo -en "${fg_orange}Uninstall keepassxc-unlock from /usr/local? (y/N) $fg_rese
 set +e
 read -r resp < /dev/tty
 set -e
-if [ "$resp" != y -a "$resp" != Y ]; then
+if ! [[ "$resp" =~ [Yy] ]]; then
   exit 1
 fi
 
 echo -e "${fg_orange}Stopping systemd services and removing the service files$fg_reset"
-for unit in $(sudo systemctl -q list-units 'keepassxc-unlock@*.service' | awk '{ print $1 }'); do
+for unit in $(sudo systemctl show 'keepassxc-unlock@*.service' --property=Id --value --no-pager | grep . | uniq); do
   echo -e "$fg_orange  Stopping service '$unit'$fg_reset"
   sudo systemctl stop "$unit"
 done
 unit=keepassxc-login-monitor.service
 echo -e "$fg_orange  Stopping service '$unit'$fg_reset"
-sudo systemctl stop "$unit" || /bin/true
+sudo systemctl stop "$unit" || true
 echo -e "$fg_orange  Disabling service '$unit'$fg_reset"
-sudo systemctl disable "$unit" || /bin/true
+sudo systemctl disable "$unit" || true
 for file in $service_files; do
-  sudo rm -f /etc/systemd/system/$file
+  sudo rm -f "/etc/systemd/system/$file"
 done
 echo -e "${fg_orange}Reloading systemd daemon$fg_reset"
 sudo systemctl daemon-reload
 
 echo -e "${fg_orange}Removing executables from /usr/local/sbin$fg_reset"
-for file in $sbin_files $old_sbin_files; do
-  sudo rm -f /usr/local/sbin/$file
+for file in $sbin_files  $old_sbin_files; do
+  sudo rm -f "/usr/local/sbin/$file"
 done
 
 echo -e "${fg_orange}Removing LICENSE and doc files from /usr/local/share/doc$fg_reset"
-sudo rm -rf /usr/local/share/doc/keepassxc-unlock /usr/local/share/doc/$old_package
+sudo rm -rf /usr/local/share/doc/keepassxc-unlock "/usr/local/share/doc/$old_package"
 
-if [ -d $config_dir ]; then
+if [[ -d "$config_dir" ]]; then
   echo
   echo -e "${fg_cyan}Should the KeePassXC database configuration in $config_dir be removed?"
   echo -n "Be warned that if you remove it, then all the KeePassXC database passwords registered"
@@ -59,7 +68,7 @@ if [ -d $config_dir ]; then
   set +e
   read -r resp < /dev/tty
   set -e
-  if [ "$resp" = YES ]; then
+  if [[ "$resp" == YES ]]; then
     echo -e "${fg_orange}Removing /etc/keepassxc-unlock$fg_reset"
     sudo rm -rf /etc/keepassxc-unlock
   fi
@@ -67,4 +76,4 @@ fi
 
 echo
 echo -e "${fg_green}Uninstalled keepassxc-unlock."
-echo -e $fg_reset
+echo -e "$fg_reset"
