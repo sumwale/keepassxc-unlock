6c2feadb77d4228c8a052f9970e1e3a05833abea
laur89 <laur89@users.noreply.github.com>
2026-01-30 17:06:14 +0100

    install.sh: swallow find error on non-existing /etc/keepassxc-unlock (#12)

--- install.sh
+++ install.sh
@@ -182,7 +182,7 @@ while IFS= read -r -d $'\0' old_conf; do
       fi
     fi
   fi
-done < <(sudo find /etc/keepassxc-unlock -maxdepth 2 -mindepth 2 -name '*.conf' -print0)
+done < <(sudo find /etc/keepassxc-unlock -maxdepth 2 -mindepth 2 -name '*.conf' -print0 2>/dev/null)
 
 echo
 echo -e "${fg_orange}Start user-specific auto-unlock service? This will only work if"
